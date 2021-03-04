383602c9631382f92e05bd2f932b11f7910038de
alicefritz <alicejfritz@gmail.com>
2021-03-04 16:08:15 +0100

    updated port to use for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,6 +18,6 @@ app.get("/", (req, res) => {
 require("./app/routes/thought.routes.js")(app);
 
 // set port, listen for requests
-app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
 });

377097152301789aeeac02384c648f567203ff95
alicefritz <alicejfritz@gmail.com>
2021-03-08 11:06:36 +0100

    cors should be enabled now

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.use(bodyParser.json());
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "thoughts-api" });
